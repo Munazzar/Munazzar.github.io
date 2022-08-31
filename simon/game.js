152de515239961c434359e5bf0397d01a955120e
Munazzaruddin M <mdmunazzar@hotmail.com>
2022-08-31 10:38:35 -0500

    Add files via upload

---
+++ simon/game.js
@@ -0,0 +1,73 @@
+var buttonColours = ["red", "blue", "green", "yellow"]; 
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var indexCheck = 0;
+
+function nextSequence(){
+
+    level+=1;
+    $("h1").text("Level "+level);
+    var randomNumber = Math.floor(Math.random() * 4);
+    // console.log(randomNumber);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    playSound(randomChosenColour);
+    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
+    
+}
+
+
+
+// nextSequence();
+// console.log(gamePattern);
+
+$("body").keypress(function(){
+   
+    if(level==0)
+    {
+        nextSequence();
+    }
+    
+
+});
+
+$(".btn").on("click",function(){
+    // console.log("hi");
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    if(gamePattern[indexCheck] == userChosenColour ){
+        indexCheck+=1;
+        if(indexCheck == gamePattern.length){
+            indexCheck = 0;
+            setTimeout(function(){
+                nextSequence();
+              }, 1000);
+           
+        }
+    }
+    else{
+        indexCheck = 0;
+        gamePattern = [];
+        level = 0;
+        $("h1").text("Game Over, Press Any Key to Restart");
+        userChosenColour = "wrong";
+    }
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
+function playSound(name){
+    const audioElement = new Audio(`sounds/${name}.mp3`);
+    audioElement.play();
+    
+   
+}
+    
+function animatePress(currentColour){
+    $(`#${currentColour}`).addClass("pressed").delay(100);
+    setTimeout(function(){
+        $(`#${currentColour}`).removeClass("pressed");
+      }, 100);
+    // $(`#${currentColour}`).removeClass("pressed");
+}
